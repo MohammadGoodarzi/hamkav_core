22a3dc323dce5406080a9af6a475f6d143f5330c
mg <mr.mgoodarzi@gmail.com>
2023-10-13 12:10:45 +0330

    for enable in remote serve

--- mentolink/settings/base.py
+++ mentolink/settings/base.py
@@ -159,7 +159,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hamkav_dash_db',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'INcaYqhfvUvnXcXnRYymBveW',
         'HOST': 'localhost',
         'PORT': '5432',
     }
